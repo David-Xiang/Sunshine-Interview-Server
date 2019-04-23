8d756f9bdfff8ba406930de13f88668be4c3b633
David Xiang <xiangdongwei@pku.edu.cn>
2019-04-23 13:50:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 "use strict";
+let dbhost = "10.2.147.123";
 let http = require("http");
 let url = require("url");
 let os = require("os");
 let fs = require("fs");
+let path = require("path");
 let formidable = require("formidable");
-let dbmodule = require("./db_connect.js");
-let dbconnect = new dbmodule("10.2.147.123");
+let mime = require("./mime").types;
+let dbmodule = require("./db_connect");
+let dbconnect = new dbmodule(dbhost);
+
 
 /** 
  * Utilities
@@ -31,71 +35,201 @@ let server = http.createServer(async function(req, res) {
 
     let pathname = url.parse(req.url).pathname;
     let query = {};
-    if (url.parse(req.url).query !== null)
+    let pathinfo = {};
+    if (url.parse(req.url).query !== null){
         query = parseQueryString(url.parse(req.url).query);
+    }
+    pathinfo = parsePathString(url.parse(req.url).pathname);
     
     let result = {};
-    switch(pathname){
+    switch(pathinfo.type){
         case "/":
             result = illegalRequest;
+            responseJson(res, result);
             break;
         case "/validate": 
             result = await getInterviewInfo(query["siteid"], query["validatecode"]);
+            responseJson(res, result);
             break;
         case "/side":
             result = await chooseSide(query["siteid"], query["side"]);
+            responseJson(res, result);
             break;
         case "/order":
             result = await chooseOrder(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/teacher":
             result = await teacherSignin(query["siteid"], query["order"], query["id"]);
+            responseJson(res, result);
             break;
         case "/querystudent":
             result = await queryStudent(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/start":
             result = await start(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/end":
             result = await end(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/queryorder":
             result = await queryOrder(query["siteid"]);
+            responseJson(res, result);
             break;
         case "/student":
             result = await studentSignin(query["siteid"], query["order"], query["id"]);
+            responseJson(res, result);
             break;
         case "/querystart":
             result = await queryStart(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/queryend":
             result = await queryEnd(query["siteid"], query["order"]);
+            responseJson(res, result);
             break;
         case "/upload":
-            result = handleUpload(req, res, query["id"]);
+            result = handleUpload(req, res, pathinfo.path);
+            responseJson(res, result);
             break;
         case "/download":
-            await handleDownload(req, res, query["filename"], "./images/");
-            return;
+            await handleDownload(res, "./files/"+pathinfo.path);
+            // TODO
+            break;
         case "/reset":
             // Backdoor: reset database
             // DELETE BEFORE RELEASE!!!
             await dbconnect.cleanData();
+            responseJson(res, result);
+            break;
+        case "/static":
+            handleSite(res, "./site/static" + pathinfo.path);
+            break;
+        case "/site":
+            handleSite(res, "./site" + pathinfo.path);
+            // TODO
+            break;
+        case "/login":
+            handleLogin(res, query["username"], query["password"]);
+            //responseJson(res, permission);
+            break;
+        case "/register":
+            // TOKEN
+            handleRegister(req, res);
+            break;
         default:
             result = illegalRequest;
+            responseJson(res, result);
+            break;
     }
-    
-    res.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
-    res.write(JSON.stringify(result));
-    res.end();
 });
 
 server.listen(80);
 console.log("Server starts on port " + 80);
 showLocalIP();
 
-function handleUpload(req, res, id){
+function responseJson(res, json){
+    responseText(res, JSON.stringify(json));
+}
+
+function responseText(res, textString){
+    res.writeHead(200, {"Content-Type": "text/plain;charset=utf-8"});
+    res.write(textString);
+    res.end();
+}
+
+function responseError(res, text){
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write(text);
+    res.end();
+}
+
+async function handleLogin(res, username, password){
+    if (typeof(username) !== 'string' || typeof(password) !== "string")
+        responseJson(res, illegalRequest);
+    let dbResult = await dbconnect.webValidateFromDB(username, password);
+    //let dbResult = { legal : "true", CollegeID : "66"};
+    if (dbResult.legal === 'true'){
+        let result = permission;
+        result.CollegeID = dbResult.CollegeID;
+        responseJson(res, result);
+    } else {
+        responseJson(res, illegalRequest);
+    }
+}
+
+async function handleRegister(req, res){
+    console.log("Enter register");
+    if (req.method.toLowerCase() !== "post"){
+        console.log("not post");
+        console.log(req.method);
+        return denyPermission;
+    }
+    let dataStr = "";
+    req.on('data', function (chunk) {
+        dataStr += chunk;
+        //console.log("DATA");
+        //console.log(dataStr);
+    });
+    req.on('end', async function(){
+        dataStr = decodeURI(dataStr);
+        console.log("Register:");
+        console.log(dataStr);
+        console.log("END");
+        responseJson(res, permission);
+        let data = JSON.parse(dataStr);
+        let collegeID = null;
+        if (data.hasOwnProperty("teacher")){
+            let arr = data.teacher;
+            for (item in arr){
+                collegeID = item.CollegeID;
+                let res = await dbconnect.insertStudentTakesToDB(
+                                item.CollegeID, 
+                                item.InterviewSiteName, 
+                                item.StartTime, 
+                                item.EndTime, 
+                                item.TeacherID, 
+                                item.TeacherName, 
+                                item.Email, 
+                                item.PhoneNumber,
+                                item.DeptName);
+                if (res.legal === false){
+                    console.log("Error in inserting teachers' info!");
+                    return;
+                }
+            }
+        }
+        if (data.hasOwnProperty("student")){
+            let arr = data.student;
+            for (item in arr){
+                collegeID = item.CollegeID;
+                let res = await dbconnect.insertStudentTakesToDB(
+                                item.CollegeID, 
+                                item.InterviewSiteName, 
+                                item.StartTime, 
+                                item.EndTime, 
+                                item.StudentID, 
+                                item.StudentName, 
+                                item.Email, 
+                                item.PhoneNumber);
+                if (res.legal === false){
+                    console.log("Error in inserting students' info!");
+                    return;
+                }
+            }
+        }
+        if (collegeID !== null){
+            let res = await dbconnect.buildTablesToDB(collegeID);
+            console.log("Building Table:");
+            console.log(res);
+        }
+    });
+}
+
+function handleUpload(req, id){
     if (!id)
         return illegalRequest;
     if (req.method.toLowerCase() !== "post")
@@ -103,7 +237,7 @@ function handleUpload(req, res, id){
 
     let form = new formidable.IncomingForm();
     form.encoding = 'utf-8';
-    form.uploadDir = "./origin";
+    form.uploadDir = "./files";
     form.keepExtensions = true;
     form.parse(req, function(err, fields, files) {
         // rename file
@@ -120,16 +254,36 @@ function handleUpload(req, res, id){
     return {"type": "upload_info", "status": "success"};
 }
 
-async function handleDownload(req, res, fileName, filePath){
-    if (!fileName)
-        return illegalRequest;
-    if (! await fs.existsSync(filePath+fileName))
+function handleSite(res, realpath){
+    fs.exists(realpath, function(exist){
+        if (!exist){
+            responseError(res, "This request URL " + realpath + " was not found on this server.");
+            return;
+        }
+        fs.readFile(realpath, "binary", function(err, file){
+            if (err){
+                responseError(res, JSON.stringify(err));
+                return;
+            }
+    
+            let ext = path.extname(realpath);
+            ext = ext ? ext.slice(1) : 'unknown';
+            let contentType = mime[ext] || "text/plain";
+            res.writeHead(200, {'Content-Type': contentType});
+            res.write(file, "binary");
+            res.end();
+        });
+    });
+}
+
+async function handleDownload(res, filePath){
+    if (!await fs.existsSync(filePath))
         return illegalRequest;
     res.writeHead(200,{  
         'Content-Type': 'application/octet-stream', //告诉浏览器这是一个二进制文件  
-        'Content-Disposition': 'attachment; filename=' + fileName, //告诉浏览器这是一个需要下载的文件  
+        'Content-Disposition': 'attachment; filename=' + path.basename(filePath) //告诉浏览器这是一个需要下载的文件  
     });
-    fs.createReadStream(filePath+fileName).pipe(res);
+    fs.createReadStream(filePath).pipe(res);
 }
 
 function showLocalIP(){
@@ -152,6 +306,16 @@ function parseQueryString(queryString){
         query[key] = value;
     });
     return query;
+}
+
+function parsePathString(pathString){
+    let info = {};
+    let pathIndex = pathString.indexOf("/", 1);
+    if (pathIndex === -1)
+        pathIndex = pathString.length;
+    info.type = pathString.substring(0, pathIndex);
+    info.path = pathString.substring(pathIndex);
+    return info;
 }
 
 /**
@@ -327,3 +491,6 @@ async function queryEnd(siteId, order){
     }
     return denyPermission;
 }
+
+//login : webValidate => result + collID
+//新建 : newSchmea(ID), insert, build
